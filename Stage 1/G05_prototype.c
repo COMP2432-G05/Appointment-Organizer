145d2b8b377616f4e278339aed75c581dbc51796
David Jiang <jiangguanlin2003@outlook.com>
2023-02-22 17:39:18 +0800

    Update Stage 1

--- Stage 1/G05_prototype.c
+++ Stage 1/G05_prototype.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    printf("parent process %d:\n", getpid());
+
+	for(int i = 3; i < argc; i++) {
+		if(fork() == 0) {
+			printf("child process %d, from User %s\n",getpid(), argv[i]);
+			exit(0);
+		}
+        sleep(2);
+        
+	}
+
+    for (int i = 3; i < argc; i++) {
+        wait(NULL);
+    }
     
-
-    return 0;
+    return 0; 
 }
