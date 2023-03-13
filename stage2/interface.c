5de884bddeb984ff82ee671d36eea6277f086a67
David Jiang <jiangguanlin2003@outlook.com>
2023-03-13 20:30:12 +0800

    Update

---
+++ stage2/interface.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    
+
+    return 0;
+}
+
+// trying !!!
+// struct Appointment {
+//     char[] type;
+//     int time;
+//     int date;
+//     float timeRange;
+//     char[] callers;
+// }
+
+// input module for user input
+void inputModule() {
+
+}
+
+// input module for file input
+void inputModuleForBatch() {
+
+}
+
+// call the functions by partent by user input [from program]
+// identify varible --> is the first statement user input
+void judgeInputStatement(char identify, char others) {
+    switch(identify) {
+        case 'privateTime':
+
+        break;
+        case 'projectMeeting':
+   
+        break;
+
+        case 'groupStudy':
+   
+        break;
+
+        case 'gathering':
+   
+        break;
+
+        case 'printSchd':
+   
+        break;
+
+        case 'endProgram':
+   
+        break;
+    }
+}
+
+// scheduling the appointment [from user]
+void scheduling() {
+
+}
+
+// scheduling kernel - use at least 2 scheduling algorithms [from program]
+void schedulingKernel() {
+
+}
+
+void schedulingAlgorithm1() {
+
+}
+
+void schedulingAlgorithm2() {
+
+}
+
+// checking the appointment time range is avaible or not [from program]
+int timeRangeChecking() {
+
+}
+
+void rescheduling() {
+
+}
+
+// cancel the appointment
+void cancelAppointment() {
+
+}
+
+// create .txt file & store the records to the file
+void logRecords() {
+
+}
+
+void logAppointmentSchedule() {
+
+}
+
+// log the rejected list and attach the reason why reject
+void logRejectedList() {
+
+}
+
+// print the analyze performance report for user
+void performanceReport() {
+
+}
