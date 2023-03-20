0c1d84d1ffb37ddd72550c15e6b750d0b5d86dcd
David Jiang <jiangguanlin2003@outlook.com>
2023-03-20 21:19:22 +0800

    Update interface.c
    Co-Authored-By: AlexLiuyz <117089712+AlexLiuyz@users.noreply.github.com>
    Co-Authored-By: FranklinLT <117089555+FranklinLT@users.noreply.github.com>
    Co-Authored-By: Noel2002 <64746276+Noel2002@users.noreply.github.com>
    Co-Authored-By: Vinct-852 <113018465+Vinct-852@users.noreply.github.com>

--- stage2/interface.c
+++ stage2/interface.c
@@ -1,26 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    
+    
+    // start the program
+    int i;
+    int childNumber = argc-3; 
+    // names list
+    char(*name)[20] = (char(*)[20])malloc(sizeof(char) * (argc - 3) * 20);
+    for(i = 0; i < childNumber; i++){
+        strcpy(name[i], argv[i+3]);
+    }
+    char startDate[8];
+    char endDate[8];
+    strcpy(startDate, argv[1]);
+    strcpy(endDate, argv[2]);
+
+    if(childNumber < 3 || childNumber > 10) printf("*the number of users is not in the range 3 to 10*\n"); exit(1);//The system should return an error message if the number of users is not in the range 3 to 10
+    
+    //pipe
+    int p2c[childNumber][2];	// parent to child
+    int c2p[childNumber][2];    // child to parent
+    char buf[80], buf2[80];
+
+    //pipe error 
+    for(i=0;i<childNumber;i++){
+        if (pipe(&p2c[i][0]) < 0) {
+            printf("Pipe creation error\n");
+            exit(1);
+        }
+        if (pipe(&c2p[i][0]) < 0) {
+            printf("Pipe creation error\n");
+            exit(1);
+        }
+    }
+
+    printf("parent process %d:\n", getpid());
 
     
+	for(i = 0; i < childNumber; i++) {
+        pid_t childId = fork();
+        
+        if(childId < 0) {
+            printf("*Create child process falied*\n");
+            exit(1);
+        }    
+		else if(childId == 0) {
+			printf("child process %d, from User %s\n",getpid(), name[i]);
+            while(true){
+                read(p2c[i])
+            }
+			exit(0);
+		}
+        else if(childId > 0) {
+
+        }
+	}
+    
+    // Program start
+    char input[100];
+    printf("~~WELCOME TO APO~~\n");
+    
+    while(1){ 
+        printf("Please enter appointment:\n");
+        gets(input);
+        judgeInputStatement(input);
+        
+        if(strcmp("endProgram", input) == 0) break; // end Program
+    }
+    
+    for (i = 0; i < childNumber; i++) { 
+        wait(NULL);
+    }
+    printf("parent jobs finished!!\n");
 
     return 0;
 }
 
 // trying !!!
 // struct Appointment {
-//     char[] type;
-//     int time;
+//     char[][] type;
+//     int startTime;
 //     int date;
-//     float timeRange;
-//     char[] callers;
 // }
 
 // input module for user input
 void inputModule() {
-
+    
 }
 
 // input module for file input
@@ -33,44 +102,63 @@ void inputModuleForBatch() {
 void judgeInputStatement(char identify, char others) {
     switch(identify) {
         case 'privateTime':
+            printf("privateTime");
 
         break;
+
         case 'projectMeeting':
-   
+            printf("projectMeeting");
         break;
 
-        case 'groupStudy':
-   
+        case 'groupStudy': 
+            printf("groupStudy");
         break;
-
+            
         case 'gathering':
-   
+            printf("gathering");
         break;
 
         case 'printSchd':
-   
+            printf("printSchd");
         break;
 
         case 'endProgram':
-   
+            printf("endProgram");
         break;
     }
 }
+
+
 
 // scheduling the appointment [from user]
 void scheduling() {
 
 }
 
-// scheduling kernel - use at least 2 scheduling algorithms [from program]
+// Scheduling kernel - use at least 2 scheduling algorithms [from program]
 void schedulingKernel() {
 
 }
 
-void schedulingAlgorithm1() {
+// FCFS Scheduling Algorithm
+void schedulingAlgorithm1(float[] burstTime, int[] arriveTime, int[] peopleId) {
+    int i;
+    int j;
+    char queue[];
+    for(i = 0; i < n; i++) {
+        printf("P[%d]:", processID[i]);
+    }
 
+    read(p[0], msg, MSGSIZE);
+    for (i = 0; i < sizeof(peopleId); i++) {
+        if (msg == peopleId[i]) {
+            scheduling();
+        }
+    }
+    
 }
 
+// Priority Scheduling Algorithm
 void schedulingAlgorithm2() {
 
 }
@@ -84,10 +172,14 @@ void rescheduling() {
 
 }
 
+
+
 // cancel the appointment
 void cancelAppointment() {
 
 }
+
+
 
 // create .txt file & store the records to the file
 void logRecords() {
